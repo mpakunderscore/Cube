7d1abf93e0c7a34f898d89aab413e4794a7a0631
mpakunderscore <mpakunderscore@gmail.com>
2018-03-10 02:08:35 +0300

    Stable time

--- js/game.js
+++ js/game.js
@@ -1,18 +1,54 @@
 let game = false;
 let time = 0;
 
-let total = 1800;
+let totalDefault = 1800;
+let totalCurrent = totalDefault;
 
 function startGame() {
+
+    if (game)
+        return;
+
+    log('start game')
 
     game = true;
     time = 0;
 
     $('#progress > div').css('width', '0');
 
-    $('#total').text(sec(total));
+    totalCurrent = totalDefault;
+
+    $('#total').text(fromSec(totalCurrent));
+
+    $('#start').addClass('off');
+
+    $('#logo').addClass('rotating');
+
+    // $('#start').css('box-shadow', 'none');
 
     startGameTime();
+}
+
+function stopGame() {
+
+    log('stop game')
+
+    game = false;
+
+    $('#start').removeClass('off');
+
+    $('#logo').removeClass('rotating');
+}
+
+function resetGame() {
+
+    stopGame();
+
+    log('reset game')
+
+    time = 0;
+
+    $('#current').text(fromSec(time));
 }
 
 function startGameTime() {
@@ -25,12 +61,13 @@ function startGameTime() {
 
         time = time + 1;
 
-        if (time > total) {
+        if (time > totalCurrent) {
 
+            log('time end')
 
+            stopGame();
             return;
         }
-
 
         setTimeout(function() {
             startGameTime()
@@ -40,10 +77,29 @@ function startGameTime() {
 
 function updateTime() {
 
-    $('#current').text(sec(time));
+    $('#current').text(fromSec(time));
 }
 
-function sec(time) {
+function addTime() {
+
+    log('add time: ' + fromSec(totalCurrent) + ' + ' + $('#addTimeField').text())
+
+    totalCurrent = totalCurrent + toSec($('#addTimeField').text());
+
+    $('#total').text(fromSec(totalCurrent));
+}
+
+function setTime() {
+
+    totalDefault = toSec($('#setTimeField').text());
+    totalCurrent = totalDefault;
+
+    log('set total time: ' + fromSec(totalDefault))
+
+    $('#total').text(fromSec(totalDefault));
+}
+
+function fromSec(time) {
 
     let sec = '00';
 
@@ -54,4 +110,16 @@ function sec(time) {
         sec = time%60;
 
     return Math.floor(time/60) + ':' + sec;
+}
+
+function toSec(time) {
+
+    console.log(time)
+    console.log(time.split(":"));
+
+    let sec = (time.split(":")[0] * 60) * 1 + (time.split(":")[1]) * 1;
+
+    console.log(sec)
+
+    return sec;
 }
