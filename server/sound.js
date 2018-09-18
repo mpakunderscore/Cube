bb3215a5fdfa78370d0c19168ab4eb6f8550cd96
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-09-18 16:09:01 +0300

    Add button fix

--- server/sound.js
+++ server/sound.js
@@ -1,66 +1,29 @@
-// const player = require('play-sound')(opts = {})
-
 let api = require('./api');
-
-// let Omx = require('node-omxplayer');
 
 let OMXPlayer = require('omxplayer');
 
-let configuration = {};
-let omxplayer = new OMXPlayer(configuration);
-
-// let sounds = ['one']
-
-// const audioPlay = require('audio-play');
-// const audioLoad = require('audio-loader');
+let sounds = [];
 
 exports.play = function (name) {
 
     api.broadcastLog('play ' + name);
 
-    // Create an instance of the player with the source.
-    // let player = Omx('./sounds/' + name, 'local', 100);
+    let configuration = {};
+    let omxplayer = new OMXPlayer(configuration);
+    omxplayer.start('/home/pi/cube/sounds/' + name, function (error) {});
 
-    // player.play();
-
-    omxplayer.start('/home/pi/cube/sounds/' + name, function(error) {});
+    sounds.push(omxplayer);
 };
 
 exports.stop = function () {
 
-    omxplayer.stop();
+    for (let i = 0; i < sounds.length; i++) {
+        sounds[i].stop();
+    }
+
+    sounds = [];
 };
 
-omxplayer.start('/home/pi/cube/sounds/' + 'ufo.mp3', function(error) {});
-
-
-// let api = require('./api');
-//
-// let OMXPlayer = require('omxplayer');
-//
-// exports.play = function (name, volume) {
-//
-//     if (!volume)
-//         volume = 100;
-//
-//     api.broadcastLog('play ' + name);
-//     let configuration = {Volume: volume};
-//     let omxplayer = new OMXPlayer(configuration);
-//     omxplayer.start('/home/pi/cube/sounds/' + name, function(error) {});
-// };
-//
-// exports.play("ufo.mp3");
-// exports.play("ufo.mp3", 50);
-//
-// let OMXPlayer = require('omxplayer');
-// let configuration = {};
-// let omxplayer = new OMXPlayer(configuration);
-// exports.play = function (name, volume) {
-//
-//     // omxplayer.setVolume(volume, function(error) {});
-//     omxplayer.start('/home/pi/cube/sounds/' + name, {volume: 0}, function(error) {});
-// };
-// exports.play("ufo.mp3", 0.1);
-
-
-
+let configuration = {};
+let omxplayer = new OMXPlayer(configuration);
+omxplayer.start('/home/pi/cube/sounds/ufo.mp3', function (error) {});
